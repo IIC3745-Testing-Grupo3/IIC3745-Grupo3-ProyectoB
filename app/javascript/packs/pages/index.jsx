8dc2d32d4595f98f3d5027b2e5ab8ab714143118
meretamal <meretamal@uc.cl>
2021-11-20 23:29:00 -0300

    feat: add a navbar component

--- app/javascript/packs/pages/index.jsx
+++ app/javascript/packs/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import Navbar from '../components/layout/Navbar';
 
 export default function Pages() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
