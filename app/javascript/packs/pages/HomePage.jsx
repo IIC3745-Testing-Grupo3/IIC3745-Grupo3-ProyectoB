cb2979ac5c6dbed46c0029b020f3eb13bf37c186
meretamal <meretamal@uc.cl>
2021-11-20 23:29:26 -0300

    feat(home): add a button to create a new movie

--- app/javascript/packs/pages/HomePage.jsx
+++ app/javascript/packs/pages/HomePage.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Add from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
 
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <Container sx={{ pt: 4 }}>
+      <Grid container justifyContent="space-between" alignItems="flex-end">
+        <Grid item>
+          <Typography variant="h4">Películas</Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" endIcon={<Add />}>
+            Agregar película
+          </Button>
+        </Grid>
+      </Grid>
+      <Divider sx={{ my: 2 }} />
+    </Container>
+  );
 }
