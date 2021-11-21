d022a2bfbe6b5e7d13820611a3e1a8b55387cbc9
meretamal <meretamal@uc.cl>
2021-11-21 09:28:48 -0300

    feat: add breadcrumbs to improve navigation

--- app/javascript/packs/pages/NewMoviePage.jsx
+++ app/javascript/packs/pages/NewMoviePage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import { useSelector } from 'react-redux';
+import { Link as RouterLink } from 'react-router-dom';
 import NewMovieForm from '../components/movies/NewMovieForm';
 import NewMovieScreenings from '../components/movies/NewMovieScreenings';
 import NewMovieSteps from '../components/movies/NewMovieSteps';
@@ -10,6 +13,12 @@ export default function NewMoviePage() {
   const currentStep = useSelector((state) => state.movies.newMovieStep);
   return (
     <>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" to="/" component={RouterLink}>
+          Inicio
+        </Link>
+        <Typography color="text.primary">Nueva película</Typography>
+      </Breadcrumbs>
       <Typography variant="h4">Nueva película</Typography>
       <Box sx={{ width: 0.5, my: 4 }}>
         <NewMovieSteps />
