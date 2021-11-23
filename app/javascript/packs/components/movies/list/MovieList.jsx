45dbb50e1c55fd489b0590da6822a4f44c67e7f2
humbertoortuzar <hjortuzar@uc.cl>
2021-11-23 17:02:59 -0300

    feat(movies): add movie list to homepage

---
+++ app/javascript/packs/components/movies/list/MovieList.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import MovieListItem from './MovieListItem';
+import Loading from '../../ui/Loading';
+import { useGetMoviesQuery } from '../../../api/moviesApi';
+
+export default function BoxComponent() {
+  const { data, isLoading } = useGetMoviesQuery();
+  if (isLoading) {
+    return <Loading />;
+  }
+  return (
+    <Grid container spacing={2} justifyContent="center">
+      {data.map((movie) => (
+        <Grid item key={movie.id} lg={3} md={6} xs={12}>
+          <MovieListItem movie={movie} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
