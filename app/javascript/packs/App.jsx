4de258e70f30e87c588ca728ac46f3fa60f832e0
meretamal <meretamal@uc.cl>
2021-11-24 16:40:22 -0300

    feat: add responsive font sizes

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
 import Pages from './pages';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Pages />
-    </>
+    </ThemeProvider>
   );
 }
