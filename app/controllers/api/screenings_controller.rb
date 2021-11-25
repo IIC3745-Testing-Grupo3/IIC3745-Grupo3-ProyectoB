8b9293f9a8082d490c79efce62ad0c386ec413f2
meretamal <meretamal@uc.cl>
2021-11-24 22:54:57 -0300

    feat(screenings): add an endpoint to get occupied rooms in certain dates

---
+++ app/controllers/api/screenings_controller.rb
@@ -0,0 +1,17 @@
+class Api::ScreeningsController < ApplicationController
+  def occupied
+    if !params[:start_date] or !params[:end_date]
+      return head :bad_request
+    end
+    screenings = Screening.includes(:movie).where(
+      "movies.start_date <= '#{params[:end_date]}' and movies.end_date >= '#{params[:start_date]}'"
+    ).references(:movies)
+    grouped_screenings = screenings.group_by { |screening| screening[:schedule] }
+    occupied_rooms = {
+      Matiné: grouped_screenings['Matiné'] ? grouped_screenings['Matiné'].map { |screening| screening[:room] } : [],
+      Tanda: grouped_screenings['Tanda'] ? grouped_screenings['Tanda'].map { |screening| screening[:room] } : [],
+      Noche: grouped_screenings['Noche'] ? grouped_screenings['Noche'].map { |screening| screening[:room] } : [],
+    }
+    render json: occupied_rooms
+  end
+end
