6ecfe2c08242b3378e1342c2944502aa7c19fdab
meretamal <meretamal@uc.cl>
2021-11-24 23:38:23 -0300

    feat: navigate to homepage when clicking on navbar title

--- app/javascript/packs/components/layout/Navbar.jsx
+++ app/javascript/packs/components/layout/Navbar.jsx
@@ -3,14 +3,21 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Cine
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1, cursor: 'pointer' }}
+            onClick={() => navigate('/')}
+          >
+            DCCinema
           </Typography>
         </Toolbar>
       </AppBar>
