b2bc8ce4de4255e08de86d00abfebd2824f6a867
humbertoortuzar <hjortuzar@uc.cl>
2021-11-27 19:43:05 -0300

    feat(routes): change bookings routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :movies, only: [:create, :index] do
-      resources :screenings, only: [:index]
+    resources :movies, only: [:create, :index], shallow: true do
+      resources :screenings, only: [:index], shallow: true do
+        resources :bookings, only: [:create, :index]
+      end
     end
     get '/screenings/occupied', to: 'screenings#occupied'
-    resources :screenings do
-      resources :bookings, only: [:create, :index]
-    end
   end
 
   get '/', to: 'pages#index'
