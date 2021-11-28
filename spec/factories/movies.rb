98563b3de7b41408aa469ee09be7dcf299cc8651
Diego Vásquez <dhvasquez@uc.cl>
2021-11-28 02:26:58 -0300

    feat(test): add list movie test

---
+++ spec/factories/movies.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :movie do
+    name { "Dune" }
+    poster { "https://cdn.discordapp.com/attachments/889679620457259081/913629269672996914/Dune-367914309-large.png" }
+    start_date { Date.today }
+    end_date { Date.today.advance(days: 10) }
+    created_at { DateTime.now }
+    updated_at { DateTime.now }
+
+    trait :today do
+      start_date { Date.today }
+    end
+
+    trait :tomorrow do
+      start_date { Date.today.advance(days: 1) }
+    end
+  end
+end
