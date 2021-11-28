1d8cdad082e64fe873b83037e92e668baec57a3e
Diego Vásquez <dhvasquez@uc.cl>
2021-11-28 12:49:56 -0300

    feat(test): change double to single quotation

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :movie do
-    name { "Dune" }
-    poster { "https://cdn.discordapp.com/attachments/889679620457259081/913629269672996914/Dune-367914309-large.png" }
+    name { 'Dune' }
+    poster { 'https://cdn.discordapp.com/attachments/889679620457259081/913629269672996914/Dune-367914309-large.png' }
     start_date { Date.today }
     end_date { Date.today.advance(days: 10) }
     created_at { DateTime.now }
